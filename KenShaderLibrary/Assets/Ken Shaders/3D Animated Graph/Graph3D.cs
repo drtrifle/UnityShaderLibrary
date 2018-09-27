bb88a70ce174f9fd333158cd65fc9bc43abfa190
drtrifle <drtrifle@gmail.com>
2018-09-28 01:26:19 +0800

    Added Static Graph Generator

---
+++ KenShaderLibrary/Assets/Ken Shaders/3D Animated Graph/Graph3D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph3D : MonoBehaviour {
+
+	public Transform pointPrefab;
+    [Range(10, 100)]
+    public int resolution = 10;
+
+    void Start() {
+        float step = 2f / resolution; //Clamp within -1 to 1 domain
+        Vector3 currPointPosition = Vector3.right;
+        Vector3 currPointScale = Vector3.one * step;
+
+        for ( int i = 0; i < resolution; i++) {
+            Transform pointTransform = Instantiate(pointPrefab);
+            currPointPosition.x = (i + 0.5f) * step - 1f;
+            currPointPosition.y = currPointPosition.x * currPointPosition.x * currPointPosition.x;
+            pointTransform.position = currPointPosition;
+            pointTransform.localScale = currPointScale;
+            pointTransform.SetParent(transform, false);
+        }
+    }
+}
